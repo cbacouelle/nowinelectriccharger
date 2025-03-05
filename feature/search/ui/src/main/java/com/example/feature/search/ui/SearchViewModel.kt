f560184c6eb3f324ede8bfe32d0c015c3ecec269
Clement Bacouelle <c.bacouelle@betclicgroup.com>
2025-03-05 17:36:10 +0100

    Loading state added

--- feature/search/ui/src/main/java/com/example/feature/search/ui/SearchViewModel.kt
+++ feature/search/ui/src/main/java/com/example/feature/search/ui/SearchViewModel.kt
@@ -5,8 +5,8 @@ import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
-
 
 @HiltViewModel
 class SearchViewModel @Inject constructor() : ViewModel() {
@@ -14,9 +14,10 @@ class SearchViewModel @Inject constructor() : ViewModel() {
     private val _currentLocation = MutableStateFlow<Location?>(null)
     val currentLocation = _currentLocation.asStateFlow();
 
+    val locationLoading = currentLocation.map { it == null }
+
     fun setCurrentLocation(location: Location) {
         _currentLocation.value = location;
     }
-
 
 }
