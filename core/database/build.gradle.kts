438768a03308ada3ec69ffeb698156675010f02a
Clement Bacouelle <c.bacouelle@betclicgroup.com>
2025-03-20 10:10:48 +0100

    Remove android manifest and TUI and Unit test

--- core/database/build.gradle.kts
+++ core/database/build.gradle.kts
@@ -21,15 +21,6 @@ android {
         consumerProguardFiles("consumer-rules.pro")
     }
 
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
